daee5e9d42619ffee7394de86b65799e733e8a44
Adeleke Jubril <98592769+Jubbic@users.noreply.github.com>
2026-07-19 10:46:26 +0100

    Add customer spending report SQL query

--- sql_scripts/06_business_challenges.sql
+++ sql_scripts/06_business_challenges.sql
@@ -1,1 +1,16 @@
--- Business Challenges
+-- Customer Spending Report --
+
+select
+c.customer_name,
+c.city,
+sum(oi.quantity * p.price) as total_spending,
+count(distinct o.order_id) as total_order,
+rank() over(order by sum(oi.quantity * p.price) desc) as spending_rank
+from customers c
+join orders o
+on c.customer_id = o.customer_id
+join order_items oi
+on o.order_id = oi.order_id
+join products p
+on oi.product_id = p.product_id
+group by c.customer_name, c.city;
