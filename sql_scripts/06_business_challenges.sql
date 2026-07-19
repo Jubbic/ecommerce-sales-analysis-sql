05e6cc5e51866a7f535b975fa329249b752112e3
Adeleke Jubril <98592769+Jubbic@users.noreply.github.com>
2026-07-19 10:50:03 +0100

    Enhance SQL script with sales trend analysis
    Added Monthly Sales Trend Analysis to the SQL script.

--- sql_scripts/06_business_challenges.sql
+++ sql_scripts/06_business_challenges.sql
@@ -15,7 +15,7 @@ join products p
 on oi.product_id = p.product_id
 group by c.customer_name, c.city;
 
--- Top Customer in Each City
+-- Top Customer in Each City --
 
 with customer_city_rank as (
 select
@@ -35,3 +35,25 @@ group by c.city, c.customer_name
 select *
 from customer_city_rank
 where city_rank = 1;
+
+-- Monthly Sales Trend Analysis --
+
+with order_revenue as (
+select
+o.order_id,
+o.order_date,
+sum(oi.quantity * p.price) as revenue
+from orders o
+join order_items oi
+on o.order_id = oi.order_id
+join products p
+on oi.product_id = p.product_id
+group by o.order_id,o.order_date
+)
+select
+order_id,
+order_date,
+revenue,
+lag(revenue) over(order by order_date) as previous_revenue,
+revenue - lag(revenue) over(order by order_date) as difference
+from order_revenue;
