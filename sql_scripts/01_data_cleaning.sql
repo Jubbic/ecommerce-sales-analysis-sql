2840250043ab7fb3d0f84af9582b879024a832ef
Adeleke Jubril <98592769+Jubbic@users.noreply.github.com>
2026-07-19 12:10:00 +0100

    Update 01_data_cleaning.sql

--- sql_scripts/01_data_cleaning.sql
+++ sql_scripts/01_data_cleaning.sql
@@ -9,10 +9,84 @@ Purpose:
 Prepare and improve data quality before analysis.
 
 Cleaning Tasks:
-- Inspect database tables
-- Identify missing values
-- Detect duplicate records
-- Check inconsistent data
-- Validate data quality
+- Data Inspection
+- Missing Value Checks
+- Duplicate Detection
+- Data Quality Validation
 =========================================
 */
+
+
+/*
+=========================================
+1. DATA INSPECTION
+=========================================
+*/
+
+-- Inspect Order Items Table
+SELECT *
+FROM order_items;
+
+
+/*
+=========================================
+2. MISSING VALUE CHECKS
+=========================================
+*/
+
+-- Check for missing customer names
+SELECT *
+FROM customers
+WHERE customer_name IS NULL;
+
+
+-- Check for missing customer cities
+SELECT *
+FROM customers
+WHERE city IS NULL;
+
+
+-- Check for missing product names
+SELECT *
+FROM products
+WHERE product_name IS NULL;
+
+
+-- Check for missing customer IDs in orders
+SELECT *
+FROM orders
+WHERE customer_id IS NULL;
+
+
+
+/*
+=========================================
+3. DUPLICATE RECORD CHECKS
+=========================================
+*/
+
+-- Check for duplicate customer names
+SELECT
+    customer_name,
+    COUNT(*) AS duplicate_count
+FROM customers
+GROUP BY customer_name
+HAVING COUNT(*) > 1;
+
+
+-- Check for duplicate product names
+SELECT
+    product_name,
+    COUNT(*) AS duplicate_count
+FROM products
+GROUP BY product_name
+HAVING COUNT(*) > 1;
+
+
+-- Check for duplicate order IDs
+SELECT
+    order_id,
+    COUNT(*) AS duplicate_count
+FROM orders
+GROUP BY order_id
+HAVING COUNT(*) > 1;
