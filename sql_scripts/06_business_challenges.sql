b03be5dc4e7b2f4ea889c7efc59185bc1f55f1dc
Adeleke Jubril <98592769+Jubbic@users.noreply.github.com>
2026-07-19 10:48:41 +0100

    Add query for top customer in each city
    Added a query to find the top customer in each city based on total spending.

--- sql_scripts/06_business_challenges.sql
+++ sql_scripts/06_business_challenges.sql
@@ -14,3 +14,24 @@ on o.order_id = oi.order_id
 join products p
 on oi.product_id = p.product_id
 group by c.customer_name, c.city;
+
+-- Top Customer in Each City
+
+with customer_city_rank as (
+select
+c.city,
+c.customer_name,
+sum(oi.quantity * p.price) as total_spending,
+rank() over(partition by city order by sum(oi.quantity * p.price) desc) as city_rank
+from customers c
+join orders o
+on c.customer_id = o.customer_id
+join order_items oi
+on o.order_id = oi.order_id
+join products p
+on oi.product_id = p.product_id
+group by c.city, c.customer_name
+)
+select *
+from customer_city_rank
+where city_rank = 1;
