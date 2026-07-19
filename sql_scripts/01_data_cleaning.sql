df1f2a1a3295c8eacb9629219acd46bea8f986e0
Adeleke Jubril <98592769+Jubbic@users.noreply.github.com>
2026-07-19 11:53:17 +0100

    Add project header to data cleaning SQL script
    Added header comment for data cleaning project.

--- sql_scripts/01_data_cleaning.sql
+++ sql_scripts/01_data_cleaning.sql
@@ -1,1 +1,18 @@
+/*
+=========================================
+DATA CLEANING PROJECT
+E-COMMERCE SALES ANALYSIS
 
+Author: Adeleke Jubril
+
+Purpose:
+Prepare and improve data quality before analysis.
+
+Cleaning Tasks:
+- Inspect database tables
+- Identify missing values
+- Detect duplicate records
+- Check inconsistent data
+- Validate data quality
+=========================================
+*/
